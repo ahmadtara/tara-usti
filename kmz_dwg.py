f6e2eece9ec4c58fa3d0efe3474309c6eb8ca534
ahmadtara <ahmadtara.pratama@gmail.com>
2025-09-24 13:59:07 +0700

    Update kmz_dwg.py

--- kmz_dwg.py
+++ kmz_dwg.py
@@ -10,7 +10,7 @@ transformer = Transformer.from_crs("EPSG:4326", "EPSG:32760", always_xy=True)
 target_folders = {
     'FDT', 'FAT', 'HP COVER', 'HP UNCOVER', 'NEW POLE 7-3', 'NEW POLE 7-4',
     'EXISTING POLE EMR 7-4', 'EXISTING POLE EMR 7-3',
-    'BOUNDARY', 'DISTRIBUTION CABLE', 'SLING WIRE', 'KOTAK', 'JALAN'
+    'BOUNDARY FAT', 'DISTRIBUTION CABLE', 'SLING WIRE', 'KOTAK', 'JALAN'
 }
 
 def extract_kmz(kmz_path, extract_dir):
@@ -89,7 +89,7 @@ def apply_offset(points_xy):
 def classify_items(items):
     classified = {name: [] for name in [
         "FDT", "FAT", "HP_COVER", "HP_UNCOVER", "NEW_POLE", "EXISTING_POLE", "POLE",
-        "BOUNDARY", "DISTRIBUTION_CABLE", "SLING_WIRE", "KOTAK", "JALAN"
+        "BOUNDARY FAT", "DISTRIBUTION_CABLE", "SLING_WIRE", "KOTAK", "JALAN"
     ]}
     for it in items:
         folder = it['folder']
@@ -105,8 +105,8 @@ def classify_items(items):
             classified["NEW_POLE"].append(it)
         elif "EXISTING" in folder or "EMR" in folder:
             classified["EXISTING_POLE"].append(it)
-        elif "BOUNDARY" in folder:
-            classified["BOUNDARY"].append(it)
+        elif "BOUNDARY FAT" in folder:
+            classified["BOUNDARY FAT"].append(it)
         elif "DISTRIBUTION CABLE" in folder:
             classified["DISTRIBUTION_CABLE"].append(it)
         elif "SLING WIRE" in folder:
@@ -169,7 +169,7 @@ def draw_to_template(classified, template_path):
                 idx += len(obj['coords'])
 
     layer_mapping = {
-        "BOUNDARY": "FAT AREA",
+        "BOUNDARY FAT": "FAT AREA",
         "DISTRIBUTION_CABLE": "FO 36 CORE",
         "SLING_WIRE": "STRAND UG",
         "KOTAK": "GARIS HOMEPASS",
@@ -304,5 +304,6 @@ def run_kmz_to_dwg():
                         st.download_button("⬇️ Download DXF", f, file_name="output_from_kmz.dxf")
             except Exception as e:
                 st.error(f"❌ Gagal memproses: {e}")
+
 
 
